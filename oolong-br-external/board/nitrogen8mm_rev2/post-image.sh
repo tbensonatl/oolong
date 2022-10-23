2f6bc5caa8438b085637fd465afc85131ee1530b
Thomas Benson <tbensongit@gmail.com>
2022-10-23 15:05:08 -0400

    Fix post-image mender artifact creation
    Move mender artifact creation from post-build to post-image
    so that it uses the most recent kernel initramfs build. The
    initramfs generation happens between the post-build and
    post-image script. Also remove errant `dd` command during
    mender artifact generation that was used for testing rollback
    logic for images that were unable to boot.

---
+++ oolong-br-external/board/nitrogen8mm_rev2/post-image.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -eu -o pipefail
+
+EXT4_IMAGE_NAME=${BINARIES_DIR}/oolong.ext4
+
+rm -rf ${BINARIES_DIR}/oolong-fs
+rm -f ${EXT4_IMAGE_NAME}
+mkdir -p ${BINARIES_DIR}/oolong-fs/boot
+cp ${BINARIES_DIR}/Image ${BINARIES_DIR}/oolong-fs/boot
+cp ${BINARIES_DIR}/oolong.dtb ${BINARIES_DIR}/oolong-fs/boot
+echo "Generating ${EXT4_IMAGE_NAME}"
+
+${HOST_DIR}/sbin/mkfs.ext4 -d ${BINARIES_DIR}/oolong-fs -r 1 -N 0 -m 5 -L "rootfs" -O 64bit ${EXT4_IMAGE_NAME} "100M"
+rm -rf ${BINARIES_DIR}/oolong-fs
+${HOST_DIR}/bin/mender-artifact write rootfs-image -t oolong-nitrogen8mm_rev2 -n dev-release -f ${EXT4_IMAGE_NAME} -o ${BINARIES_DIR}/oolong.mender
